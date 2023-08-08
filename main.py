d10ac8a2a2ff94c8a02be165a8ac4e914b18ebbb
Cody Stiffler <codystiffleraz@gmail.com>
2023-08-08 12:46:22 -0700

    Added Comments

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from data import question_data
 from quiz_brain import QuizBrain
 
 question_bank = []
-
+# Loops through dictionary and adds each input to a question bank
 for dic in question_data:
     question_bank.append(Question(dic['question'], dic['correct_answer']))
         
 quiz = QuizBrain(question_bank)
 
+# Runs the quiz
 while quiz.still_has_questions():
     quiz.next_question()
-    
+  
+# Prints final score  
 quiz.completed()
 
--- quiz_brain.py
+++ quiz_brain.py
@@ -3,17 +3,19 @@ class QuizBrain:
         self.question_number = 0
         self.question_list = question_list
         self.score = 0
-        
+    
+    # Checks to see if there is a next question, if not return False
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
         
-        
+    # Moves on to the next question 
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
         user_answer = input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
         self.check_answer(user_answer, current_question.answer)
         
+    # Checks to see if the answers are correct or incorrect
     def check_answer(self, user_answer, correct_answer):
         if user_answer.lower() == correct_answer.lower():
             self.score += 1
@@ -24,7 +26,7 @@ class QuizBrain:
         print(f"The correct answer was: {correct_answer}")
         print(f"Your current score is {self.score}/{self.question_number}")
         print("\n")
-    
+    # Prints Final Score
     def completed(self):
         print("You've completed the quiz")
         print(f"Your final score was: {self.score}/{self.question_number}")
